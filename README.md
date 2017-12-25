b1b6909dab1e61952cc8de96a4ffe3b6cc38bc79
Clément Decoodt <clement@decoodt.eu>
2017-12-25 17:21:36 +0100

    Add screen locker

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ YAUSC: Yet Another Utility Script Collection
 ============================================
 
 Collection of utility scripts I use for my computers.
+
+# lock.sh
+
+lock.sh is a custom screenlocker. It needs:
+
+ - scrot
+ - i3lock
+ - ImageMagick
+ - Hack open source font
---
+++ lock.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Script for generating a blurred lock screen
+#
+
+TEXT="Enter your password or insert your YubiKey to unlock"
+LOCK=~/tools/lock.png
+
+# Capture screen
+scrot /tmp/screen.png
+
+# Make it blurry
+convert /tmp/screen.png -blur 0x20 /tmp/screen.png
+
+# Add locked screen
+convert /tmp/screen.png \( $LOCK -background None -scale 300 \) \
+   -gravity Center -composite -matte /tmp/screen.png
+
+# Primary X offset
+convert /tmp/screen.png \
+	-font 'Hack-Bold' -pointsize 48 -fill '#FFFFFFC0' \
+	-gravity South -annotate +0+128 "$TEXT" \
+	/tmp/screen.png
+
+i3lock -i /tmp/screen.png -t -f
+rm /tmp/screen.png
